7ea7a0eae92f54aa2fde2e4bc28b15803f1287e6
Roger Tobias <celticspyder14@gmail.com>
2017-05-12 13:42:42 -0700

    adds an updated form control from checkboxes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,46 @@
 //--------------------------FRONTEND
 $(document).ready(function() {
-  event.preventDefault();
+  $("#pizzaOrderForm").submit(function(event) {
+    event.preventDefault();
+    var inputtedCrust = $("#crust").val();
+    var inputtedTopping = $("#topping").val();
 
+    var order = new Pizza(inputtedCrust, inputtedTopping);
+
+    $("#paymentDo").text(order.TotalCost());
+  });
 });
 
 //--------------------------BACKEND
+function Pizza(crust, toppings) {
+  this.crust = crust;
+  this.toppings = topping;
+};
+
+Pizza.prototype.TotalCost = function() {
+  var payment = 0;
+  if (this.crust === "regular") {
+    payment += 10.99;
+  }else if (this.crust === "large") {
+    payment += 12.99;
+  }else {
+    payment += 14.99;
+  }
+
+  if (this.topping === "1") {
+    payment += 2;
+  }else if (this.topping === "2") {
+    payment += 3;
+  }else if (this.topping === "3") {
+    payment += 4;
+  }else if (this.topping === "4") {
+    payment += 5;
+  }else if (this.topping === "5") {
+    payment += 4;
+  }else if (this.topping === "6") {
+    payment += 4;
+  }else {
+    payment += 5;
+  }
+  return payment;
+};
